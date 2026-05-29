193253162d29c7477807a4afe2ef81c83a4d0e86
Tomi Salami <salami.tomie@gmail.com>
2026-05-29 07:32:49 -0400

    added vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import keystatic from "@keystatic/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  output: "static",
+  adapter: vercel(),
   integrations: [react(), keystatic()],
   vite: {
     plugins: [tailwindcss()],
